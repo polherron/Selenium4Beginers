005b557d8a2c009e30d6e78678cb6852d133329e
polherron <pol.herron@gmail.com>
2019-05-28 13:56:30 +0100

    Added multiple browsers

--- Selenium4Beginers/Program.cs
+++ Selenium4Beginers/Program.cs
@@ -1,16 +1,52 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 
 namespace Selenium4Beginers
 {
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
+        {
+
+            IWebDriver chromeDriver = GetChromeDriver();
+            OpenPage(chromeDriver);
+
+            IWebDriver ieDriver = getIEDriver();
+            OpenPage(ieDriver);
+
+            IWebDriver firefoxDriver = getFirefoxDriver();
+            OpenPage(firefoxDriver);
+
+        }
+
+        private static IWebDriver getFirefoxDriver()
+        {
+            IWebDriver driver = new FirefoxDriver(@"c:\libraries\");
+            return driver;
+        }
+
+        private static IWebDriver getIEDriver()
+        {
+            var options = new InternetExplorerOptions
+            {
+                IgnoreZoomLevel = true
+            };
+            IWebDriver driver = new InternetExplorerDriver(@"c:\libraries\", options);
+            return driver;
+        }
+
+        private static IWebDriver GetChromeDriver()
         {
             IWebDriver driver = new ChromeDriver(@"c:\libraries\");
-            driver.Url = "http://www.google.com";
+            return driver;
+        }
 
+        public static void OpenPage(IWebDriver driver)
+        {
+            driver.Url = "http://www.google.com";
         }
     }
 }
