e3137bbf6aabd1c13a223018f2a5708ab59be3bc
polherron <pol.herron@gmail.com>
2019-05-28 13:52:06 +0100

    InitialProject

--- Selenium4Beginers/Program.cs
+++ Selenium4Beginers/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 
 namespace Selenium4Beginers
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void OpenPage()
         {
+            IWebDriver driver = new ChromeDriver(@"c:\libraries\");
+            driver.Url = "http://www.google.com";
+
         }
     }
 }
+
